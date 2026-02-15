1ea9efbd154636299aa131ac53b2e43547e66a77
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-02-15 16:29:42 -0600

    done hopefully

--- ToDo.cs
+++ ToDo.cs
@@ -1,5 +1,17 @@
-﻿namespace knightmoves;
+namespace knightmoves;
+
 public class ToDo
 {
-    // Add your code here
+    public string[] MarkAsDone(string[] todos)
+    {
+        int index = 0;
+
+        foreach (var todo in todos)
+        {
+            todos[index] = "done - " + todo;
+            index++;
+        }
+
+        return todos;
+    }
 }
